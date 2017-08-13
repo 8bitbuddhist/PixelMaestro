6759a0542467d6a0dde386b47cf7f3bae3c76889
Andre Newman <andre@8bitbuddhism.com>
2017-08-13 10:36:48 -0400

    Updated PlatformIO Arduino project.

--- arduino/README.md
+++ arduino/README.md
@@ -2,7 +2,10 @@
 This folder contains sample Arduino sketches. These sketches were created using [PlatformIO](http://platformio.org/).
 
 ## Configuring PlatformIO
-1. Open the main project directory in PlatformIO. 
+1. Open this folder in PlatformIO.
 2. Download any necessary libraries using the PlatformIO package manager.
 3. Copy the desired sketch to the `src/` folder.
-4. Build and upload the project to your board.
+4. Check that the `lib/PixelMaestro` folder links to the PixelMaestro `src/` folder. If not, create a link or copy the PixelMaestro `src/` folder into this folder.
+5. Build and upload the project to your board.
+
+Note that the lib/PixelMaestro folder is a soft link.
--- arduino/src/WS2812.cpp
+++ arduino/src/WS2812.cpp
@@ -1,9 +1,14 @@
-#include "../../src/Colors.h"
-#include "../../src/Maestro.h"
-#include "../../src/Pixel.h"
-#include "../../src/Section.h"
+#include <Arduino.h>
 
-#include "../.piolibdeps/LightWS2812_ID159/WS2812.h"
+#include <Colors.h>
+#include <Maestro.h>
+#include <Pixel.h>
+#include <Section.h>
+#include <Show.h>
+#include <show/Transition.h>
+#include <show/SectionSetColorAnimationTransition.h>
+
+#include <WS2812.h>
 
 using namespace PixelMaestro;
 
@@ -18,32 +23,43 @@ Section sections[] = {
   Section(pixels, rows, columns)
 };
 
+Show show;
+const unsigned char NUM_TRANSITIONS = 1;
+Transition *transitions[] = {
+  new SectionSetColorAnimationTransition(&sections[0], 5000, Section::ColorAnimations::NEXT, false, Section::AnimationOrientations::HORIZONTAL)
+};
+
 // WS1812 stuff
-const unsigned char LED_PIN = 5;
+const unsigned char LED_PIN = 10;
 WS2812 ws = WS2812(numPixels);
-
-cRGB RGBtoCRGB(Colors::RGB rgbColor);
-
-void setup () {
-    sections[0].setColors(Colors::COLORWHEEL, 12);
-    sections[0].setColorAnimation(Section::ColorAnimations::MERGE, false);
-    maestro.setSections(sections, 1);
-}
-
-void loop() {
-    maestro.update(millis());
-
-    for (unsigned char pixel = 0; pixel < numPixels; pixel++) {
-      ws.set_crgb_at(pixel, RGBtoCRGB(maestro.getSection(0)->getPixelColor(pixel)));
-    }
-
-    ws.sync();
-}
-
 cRGB RGBtoCRGB(Colors::RGB rgbColor) {
   cRGB cRGBColor;
   cRGBColor.r = rgbColor.r * MAX_BRIGHTNESS;
   cRGBColor.g = rgbColor.g * MAX_BRIGHTNESS;
   cRGBColor.b = rgbColor.b * MAX_BRIGHTNESS;
   return cRGBColor;
+}
+
+void setup () {
+    ws.setOutput(LED_PIN);
+    ws.setColorOrderGRB();
+
+    sections[0].setColors(Colors::COLORWHEEL, 12);
+
+    maestro.setSections(sections, 1);
+
+    show.setMaestro(&maestro);
+    show.setTiming(Show::TimingModes::RELATIVE);
+    show.setTransitions(transitions, NUM_TRANSITIONS);
+    show.toggleLooping();
+}
+
+void loop() {
+    show.update(millis());
+
+    for (unsigned char pixel = 0; pixel < numPixels; pixel++) {
+      ws.set_crgb_at(pixel, RGBtoCRGB(sections[0].getPixelColor(pixel)));
+    }
+
+    ws.sync();
 }
