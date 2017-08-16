60e993024106d9392ac22c96d1c2f630622df597
Andre Newman <andre@8bitbuddhism.com>
2017-08-16 17:19:48 -0400

    Changed Section::Overlay to pointer

--- src/show/SectionSetOverlayEvent.h
+++ src/show/SectionSetOverlayEvent.h
@@ -8,13 +8,12 @@
 namespace PixelMaestro {
 	class SectionSetOverlayEvent : public Event {
 		public:
-			SectionSetOverlayEvent(unsigned long time, Section *section, Section *overlay, Colors::MixMode mixMode, float alpha = 0.0);
+			SectionSetOverlayEvent(unsigned long time, Section *section, Section::Overlay *overlay);
+			SectionSetOverlayEvent(unsigned long time, Section *section, Section *overlaySection, Colors::MixMode mixMode, float alpha = 0.0);
 			void run();
 
 		private:
-			float alpha_;
-			Colors::MixMode mix_mode_;
-			Section *overlay_;
+			Section::Overlay *overlay_;
 			Section *section_;
 	};
 }
