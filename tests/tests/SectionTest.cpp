4ea1def9773c06f6acf793ab12a164534031f5c8
Andre Newman <andre@8bitbuddhism.com>
2017-08-16 18:34:37 -0400

    Minor updates to Section tests

--- tests/tests/SectionTest.cpp
+++ tests/tests/SectionTest.cpp
@@ -10,21 +10,28 @@ TEST_CASE("Create and manipulate a Section.", "[Section]") {
 	Pixel pixels[layout.rows * layout.columns];
 	Section section(pixels, layout.rows, layout.columns);
 	
+	int testPixelIndex = 11;
+	
 	SECTION("Verify that Pixels are set correctly.") {
-		REQUIRE(section.getPixel(11) == &pixels[11]);
+		REQUIRE(section.getPixel(testPixelIndex) == &pixels[testPixelIndex]);
+		REQUIRE(section.getNumPixels() == layout.rows * layout.columns);
+		REQUIRE(section.getLayout()->rows == layout.rows);
+		REQUIRE(section.getLayout()->columns == layout.columns);
 	}
 	
 	SECTION("Verify that Animations and Colors are set correctly.") {
 		section.setColorAnimation(Section::ColorAnimations::SOLID, false);
-		
 		section.setColors(Colors::COLORWHEEL, 12);
+		section.setCycleInterval(1000);
+		
+		// Disable fading
 		if (section.getFade()) {
 			section.toggleFade();
 		}
 		section.update(1000);
 		
-		Colors::RGB pixelColor = section.getPixelColor(11);
-		Colors::RGB targetColor = Colors::COLORWHEEL[11];
+		Colors::RGB pixelColor = section.getPixelColor(testPixelIndex);
+		Colors::RGB targetColor = Colors::COLORWHEEL[testPixelIndex];
 
 		REQUIRE((int)pixelColor.r == (int)targetColor.r);
 		REQUIRE((int)pixelColor.g == (int)targetColor.g);
