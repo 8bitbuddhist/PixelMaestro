648890d7f4b8965344282654154df7a6305f0a0d
Andre Newman <andre@8bitbuddhism.com>
2017-08-16 00:49:47 -0400

    Added Utility class test cases.

---
+++ tests/tests/UtilityTest.cpp
@@ -0,0 +1,14 @@
+#include "../catch/single_include/catch.hpp"
+#include "Utility.h"
+#include "UtilityTest.h"
+
+using namespace PixelMaestro;
+
+TEST_CASE("RNG returns a value.", "[int]") {
+	REQUIRE(Utility::rand() > 0);
+}
+
+TEST_CASE("abs_rand returns absolute value.", "[int]") {
+	char neg = -15;
+	REQUIRE(Utility::abs_char(neg) == (neg * -1));
+}
---
+++ tests/tests/UtilityTest.h
@@ -0,0 +1,14 @@
+#ifndef UTILITYTEST_H
+#define UTILITYTEST_H
+
+class UtilityTest
+{
+    public:
+        UtilityTest();
+
+    protected:
+
+    private:
+};
+
+#endif // UTILITYTEST_H
