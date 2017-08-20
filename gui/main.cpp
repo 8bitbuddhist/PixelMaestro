f5333a063716a85f00b63c33cde85582a0bf2338
Andre Newman <andre@8bitbuddhism.com>
2017-08-20 19:59:55 -0400

    Cleaned up main.cpp

--- gui/main.cpp
+++ gui/main.cpp
@@ -4,28 +4,21 @@
 #include <QFormLayout>
 #include <QVBoxLayout>
 
-#include "demo/showdemo.h"
-#include "demo/patterndemo.h"
-
 int main(int argc, char *argv[]) {
 	QApplication a(argc, argv);
 	MainWindow w;
+	MaestroController controller;
 
 	// Set default window size
-	//w.resize(400, 800);
 	w.resize(400, 400);
 
 	// Add drawing area to form
 	QVBoxLayout *mainLayout = w.findChild<QVBoxLayout*>("mainLayout");
 	Q_ASSERT(mainLayout);
 
-	MaestroController controller;
-
-	//SimpleDrawingArea *drawingArea = new SimpleDrawingArea(mainLayout->widget(), &controller);
-	//ShowDemo *drawingArea = new ShowDemo(mainLayout->widget(), &controller);
-	PatternDemo *drawingArea = new PatternDemo(mainLayout->widget(), &controller);
+	SimpleDrawingArea *drawingArea = new SimpleDrawingArea(mainLayout->widget(), &controller);
 	mainLayout->addWidget(drawingArea);
-	//mainLayout->addWidget(new MaestroControl(mainLayout->widget(), &controller));
+	mainLayout->addWidget(new MaestroControl(mainLayout->widget(), &controller));
 
 	// Enable high DPI output
 	a.setAttribute(Qt::ApplicationAttribute::AA_EnableHighDpiScaling, true);
