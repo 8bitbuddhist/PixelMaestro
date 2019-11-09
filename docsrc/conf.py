a9c45e0818a6848b94761e60efb3acfc7455b699
Andre Newman <andre@8bitbuddhism.com>
2019-11-08 23:46:13 -0500

    Regenerate docs

--- docsrc/conf.py
+++ docsrc/conf.py
@@ -57,6 +57,7 @@ html_theme_options = {
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+html_favicon = '../../../docsrc/images/logo.png'
 
 # Configure the breathe extension
 breathe_projects = {
